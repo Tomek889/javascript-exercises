3958a231d167a5ed77c4e320aa51d23b562e3097
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-08 14:57:13 +0100

    Finish exercises 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split('');
+    let finalString = '';
+    for (let i = stringArray.length - 1; i >= 0; i--) {
+        finalString += stringArray[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
