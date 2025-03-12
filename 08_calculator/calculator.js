2f4589bec3bd7518a45fb621803746a3c2e70252
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-12 21:03:40 +0100

    Finish exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, curr) => sum + curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let num of array) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1
+  for (let i = 1; i <= a; i++) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
