1f5c0566eb936e31cf73733ff4c0e1c4652f798e
Tomasz Styczeń <97025562+Tomek889@users.noreply.github.com>
2025-03-13 19:33:36 +0100

    Finish exercises 09-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const noPunctuation = string.split('').filter((char) => !(
+        char === ' ' || 
+        char === '.' || 
+        char === ',' || 
+        char === '?' || 
+        char === '!' ||
+        char === '\''
+    )).map((char) => char.toLowerCase());
 
+    let len;
+    if (noPunctuation.length % 2 === 1) {
+        len = noPunctuation.length - 1;
+    } else {
+        len = noPunctuation.length;
+    }
+
+    for (let i = 0; i < len / 2; i++) {
+        if (noPunctuation[i] !== noPunctuation[noPunctuation.length - i - 1]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let highestAge = 0;
+    let age = 0;
+    let oldestPerson;
+    for (let person of people) {
+        if (isNaN(person.yearOfDeath)) {
+            age = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+        }
+        if (age > highestAge) {
+            highestAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
